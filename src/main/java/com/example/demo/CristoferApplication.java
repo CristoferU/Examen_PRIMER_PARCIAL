887654415924ecdfc4e652ed3876a2746df13e4e
Cristofer <97177193+CristoferU@users.noreply.github.com>
2024-05-22 01:04:35 -0500

    CristoferU
    cambios

--- src/main/java/com/example/demo/CristoferApplication.java
+++ src/main/java/com/example/demo/CristoferApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CristoferApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CristoferApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CristoferApplication.class, args);
+    }
 
 }
