d6e84c93b95c1f0146e1c2c5c4470b9fedfffcdb
Cristofer <97177193+CristoferU@users.noreply.github.com>
2024-05-22 06:48:19 -0500

    CristoferU
    >:c

--- src/main/java/com/example/demo/CristoferApplication.java
+++ src/main/java/com/example/demo/CristoferApplication.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableJpaRepositories
+@EntityScan
 public class CristoferApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/repository/CristoferClienteRepository.java
+++ src/main/java/com/example/demo/repository/CristoferClienteRepository.java
@@ -2,10 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.CristoferCliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CristoferClienteRepository extends JpaRepository<CristoferCliente, Long> {
+    
     void deleteByDescripcion(String descripcion);
-    Optional<CristoferCliente> findByDescripcion(String descripcion);
+
 }
